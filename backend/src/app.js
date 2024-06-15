7c9b52017a1ad7c7cd498a968eabf678e712e853
Sabin Lohani <np03cs4s220104@heraldcollege.edu.np>
2024-06-15 21:44:47 +0545

    chore: Update CORS configuration to allow all origins

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 // app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
-app.use(cors());
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
