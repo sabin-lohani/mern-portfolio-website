09b14121c229223d140f97158d6eb426a71bee21
Sabin Lohani <np03cs4s220104@heraldcollege.edu.np>
2024-06-15 21:38:51 +0545

    refactor: Enable CORS for all origins

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,8 @@ import errorHandler from "./middlewares/errorHandler.middlewares.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
-app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+// app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(cors());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
