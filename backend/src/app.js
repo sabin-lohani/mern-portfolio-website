c890fb7fdc257be3922986971bb0d9d98773fefa
Sabin Lohani <np03cs4s220104@heraldcollege.edu.np>
2024-06-16 19:43:46 +0545

    chore: Update CORS configuration to use environment variable

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,8 +5,7 @@ import errorHandler from "./middlewares/errorHandler.middlewares.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
-// app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
