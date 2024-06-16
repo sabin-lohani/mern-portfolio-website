8fd33f05c0ec14d1e9b8d6f51a7e2f78a30666e6
Sabin Lohani <np03cs4s220104@heraldcollege.edu.np>
2024-06-16 20:42:50 +0545

    chore: Enable CORS for all origins

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,8 @@ const corsOptions = {
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization",
 };
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
